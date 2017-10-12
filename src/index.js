2d2ecc8409bf8959c17585c95208a15f7571fb32
Ramsundar Kuppusamy <ramsundar@Ramsundars-MBP.T-mobile.com>
2017-10-12 14:00:49 -0700

    use express for routing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
+const cors = require("cors")
+const express = require("express")
+const app = express();
 /**
  * HTTP Cloud Function.
  *
  * @param {Object} req Cloud Function request context.
  * @param {Object} res Cloud Function response context.
  */
-exports.helloGET = function helloGET (req, res) {
-    res.send('Hello World!');
-  };
+app.get('/messages', (req, res) =>  {
+    res.send('Hello World changed!');
+})
+
+exports.api = app;
